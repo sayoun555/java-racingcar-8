5a5d86c4ec412c8e0b8850a9b9b74bdf27eb69a4
sayoun555 <zzangsang523@gmail.com>
2025-10-26 09:06:57 +0900

    fix: 상위 예외처리 삭제

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -8,15 +8,10 @@ import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        try {
             InputView inputView = new InputView();
             OutputView outputView = new OutputView();
             RacingcarService racingcarService = new RacingcarServiceImpl();
             RacingcarController racingcarController = new RacingcarController(racingcarService, inputView, outputView);
             racingcarController.run();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            throw e;
-        }
     }
 }
