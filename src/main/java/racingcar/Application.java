8e3d9d97a69f8021441ccb7305c9ec90f04fe5c7
sayoun555 <zzangsang523@gmail.com>
2025-10-27 09:57:17 +0900

    style: Application 클래스 줄 정리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,11 +2,6 @@ package racingcar;
 
 import racingcar.config.AppConfig;
 import racingcar.controller.RacingcarController;
-import racingcar.factory.CarFactory;
-import racingcar.service.RacingcarService;
-import racingcar.service.RacingcarServiceImpl;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
