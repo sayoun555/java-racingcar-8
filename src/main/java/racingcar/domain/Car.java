cee25c73c392f970caadbf4e11a4e67e5daa0f2b
sayoun555 <zzangsang523@gmail.com>
2025-10-27 22:26:50 +0900

    fix: Car 클래스 position 필드 private로 변경

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
     private final CarName carName;
-    int position = 0;
+    private int position = 0;
 
     public Car(CarName carNameDomain) {
         this.carName = carNameDomain;
